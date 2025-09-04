7bcfd818df0cdf25127a37518e4f65c592d6b2a8
naeatammy <tamnguyendacnguyen@gmail.com>
2025-09-04 15:56:19 +0700

    Build folder structure

---
+++ event-manager/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Xin chào ReactJS 🚀</h1>
+      <p>Đây là ứng dụng đầu tiên với Vite</p>
+    </div>
+  );
+}
+
+export default App;
