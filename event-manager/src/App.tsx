7cb73a16ab4b07006642f071c00e587ea70d66ab
naeatammy <tamnguyendacnguyen@gmail.com>
2025-09-08 16:53:08 +0700

    feat: UI create new account in admin role

--- event-manager/src/App.tsx
+++ event-manager/src/App.tsx
@@ -1,10 +1,14 @@
 import AdminUsers from "./views/Admin/admin-list-account";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegisterForm from "./views/Admin/admin-create-account-form";
 function App() {
   return (
-    <div>
-      <AdminUsers />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/admin/users" element={<AdminUsers />} />
+        <Route path="/admin/users/create" element={<RegisterForm />} />
+      </Routes>
+    </Router>
   );
 }
 
