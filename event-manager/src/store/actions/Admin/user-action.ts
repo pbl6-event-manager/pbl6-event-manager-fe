40816109ad13145e5ec4d2c5a78a1d7712399a6d
naeatammy <tamnguyendacnguyen@gmail.com>
2025-10-14 15:20:05 +0700

    feat: fetch api delete/recover an account

--- event-manager/src/store/actions/Admin/user-action.ts
+++ event-manager/src/store/actions/Admin/user-action.ts
@@ -1,8 +1,10 @@
 export const FETCH_USERS = "FETCH_USERS";
 export const SET_SELECTED_USER = "SET_SELECTED_USER";
 export const CLEAR_SELECTED_USER = "CLEAR_SELECTED_USER";
+export const DELETE_USER = "DELETE_USER";
 
-import { fetchUsersService } from "../../../service/user-service";
+import { fetchUsersService, updateStatusUserService } from "../../../service/user-service";
+import { store } from "../../store";
  
 
 export const getUsers = () => async (dispatch: any) => {
@@ -13,6 +15,20 @@ export const getUsers = () => async (dispatch: any) => {
     console.error("Failed to fetch users:", error);
   }
 };
+
+export const updateStatusUser = (email: string, isActive: boolean) => async (dispatch: any) => {
+  try {
+    const deletedUser = await updateStatusUserService(email, isActive);
+    const users  = store.getState().userReducer.users;
+
+    const updatedList = users.map((user: any) =>
+      user.email === email ? { ...user, isActive: deletedUser.isActive } : user
+    );
+    dispatch({type: DELETE_USER, payload: updatedList});
+  } catch (error) {
+    console.log("Failed to delete users: ", error)
+  }
+}
 
 export const setSelectedUser = (payload: { email?: string }) => ({
   type: SET_SELECTED_USER,
