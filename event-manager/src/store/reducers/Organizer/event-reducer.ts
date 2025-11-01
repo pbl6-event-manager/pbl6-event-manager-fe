ce32a1554f322826f13979cf3252034b12ce8522
Thanhan0409 <thanhanleton123@gmail.com>
2025-11-01 12:12:30 +0700

    Feat: comment error lib

--- event-manager/src/store/reducers/Organizer/event-reducer.ts
+++ event-manager/src/store/reducers/Organizer/event-reducer.ts
@@ -1,6 +1,6 @@
 import { EVENT_ACTIONS } from "../../actions/Organizer/event-action";
 import type { EventData } from "../../../models";
-import type { CreateEventDTO } from "../../../dtos/event-dto";
+// import type { CreateEventDTO } from "../../../dtos/event-dto";
 
 export interface EventState {
     currentEvent: EventData | null
